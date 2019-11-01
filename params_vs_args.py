b50be7dc734fcd304da74311114e28ea99206f9d
Damyan Peykov <dppeykov@gmail.com>
2019-11-01 11:30:25 +0100

    Create params_vs_args.py

---
+++ params_vs_args.py
@@ -0,0 +1,7 @@
+# Parameter refers to the name we use to pass variables when DEFINING a function:
+
+def name_of_the_function(parameter1, parameter2):
+  pass
+  
+# Argument is the term used when we are passing variables on a function CALL:
+name_of_the_function(argument1, argument2)
