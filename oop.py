ceea0089189cb35336ac968adfe5c5f1c189ab1a
Damyan Peykov <dppeykov@gmail.com>
2019-11-01 08:49:31 +0100

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,5 +1,9 @@
-class Caharacter:
-    class_att = True
+# REPL: https://repl.it/@DamyanPeykov/OOP
+
+class Character:
+    # Class Object Attribute - this is not dynamic - it is static
+    # We can call it inside the class with self or with Character 
+    class_obj_att = True
     def __init__(self,name,age):
         self.name = name
         self.age = age
@@ -8,6 +12,6 @@ class Caharacter:
         return song
 
 # Instantiating a class
-singer1 = Caharacter("Tom", 42)
+singer1 = Character("Tom", 42)
 
-print(f'{singer1.name}, {singer1.age}, {singer1.sing("Blah Blah")}, {singer1.class_att}')
+print(f'{singer1.name}, {singer1.age}, {singer1.sing("Blah Blah")}, {singer1.class_obj_att}')
